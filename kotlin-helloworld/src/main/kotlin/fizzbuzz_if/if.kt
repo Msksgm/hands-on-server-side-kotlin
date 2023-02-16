1230321b8efb6d26ffe547af6779473b95dc0165
kinari321 <abcdream321@yahoo.co.jp>
2023-02-17 08:14:21 +0900

    feat: 「📃1.3 FizzBuzz を実装」の if のサンプルコードを記述

---
+++ kotlin-helloworld/src/main/kotlin/fizzbuzz_if/if.kt
@@ -0,0 +1,44 @@
+package fizzbuzz_if
+
+fun fizzBuzz1(i: Int): String {
+    if (i % 15 == 0) {
+        return "FizzBuzz"
+    } else if (i % 5 == 0) {
+        return "Fizz"
+    } else if (i % 3 == 0) {
+        return "Buzz"
+    } else {
+        return "%i"
+    }
+}
+
+fun fizzBuzz2(i: Int): String {
+    val result = if (i % 15 == 0) {
+        "FizzBuzz"
+    } else if (i % 5 == 0) {
+        "Fizz"
+    } else if (i % 3 == 0) {
+        "Buzz"
+    } else {
+        "%i"
+    }
+    return result
+}
+
+fun fizzBuzz3(i: Int): String {
+    return if (i % 15 == 0) {
+        "FizzBuzz"
+    } else if (i % 5 == 0) {
+        "Fizz"
+    } else if (i % 3 == 0) {
+        "Buzz"
+    } else {
+        "%i"
+    }
+}
+
+fun main(args: Array<String>) {
+    println(fizzBuzz1(15))
+    println(fizzBuzz2(5))
+    println(fizzBuzz3(3))
+}
