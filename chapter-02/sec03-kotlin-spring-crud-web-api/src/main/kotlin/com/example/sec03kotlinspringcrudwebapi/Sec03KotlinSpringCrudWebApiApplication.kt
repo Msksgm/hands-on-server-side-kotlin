68c58e1d7895e2d1836d300a905b6d23c9ed6ecc
kinari321 <abcdream321@yahoo.co.jp>
2023-02-19 13:24:25 +0900

    feat: 「📃2.3 Spring Boot で Web API 入門」のプロジェクト作成

---
+++ chapter-02/sec03-kotlin-spring-crud-web-api/src/main/kotlin/com/example/sec03kotlinspringcrudwebapi/Sec03KotlinSpringCrudWebApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sec03kotlinspringcrudwebapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sec03KotlinSpringCrudWebApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<Sec03KotlinSpringCrudWebApiApplication>(*args)
+}
---
+++ chapter-02/sec03-kotlin-spring-crud-web-api/src/test/kotlin/com/example/sec03kotlinspringcrudwebapi/Sec03KotlinSpringCrudWebApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sec03kotlinspringcrudwebapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Sec03KotlinSpringCrudWebApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
