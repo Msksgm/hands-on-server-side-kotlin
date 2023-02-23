04dca7fc20e27f5022e7c4679524f4d653cd8624
kinari321 <abcdream321@yahoo.co.jp>
2023-02-23 15:56:45 +0900

    feat: 「📃A.4 Spring Boot の基礎」のプロジェクト作成

---
+++ appendix-a/ch04-basic-of-spring-boot-from-kinari321/src/main/kotlin/com/example/ch04basicofspringbootfromkinari321/Ch04BasicOfSpringBootFromKinari321Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch04basicofspringbootfromkinari321
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch04BasicOfSpringBootFromKinari321Application
+
+fun main(args: Array<String>) {
+	runApplication<Ch04BasicOfSpringBootFromKinari321Application>(*args)
+}
---
+++ appendix-a/ch04-basic-of-spring-boot-from-kinari321/src/test/kotlin/com/example/ch04basicofspringbootfromkinari321/Ch04BasicOfSpringBootFromKinari321ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ch04basicofspringbootfromkinari321
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ch04BasicOfSpringBootFromKinari321ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
