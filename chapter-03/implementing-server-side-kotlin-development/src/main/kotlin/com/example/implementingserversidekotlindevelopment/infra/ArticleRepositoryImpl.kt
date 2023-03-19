3433e04a58ed14e3ec348ba8837fab580ede617f
kinari321 <abcdream321@yahoo.co.jp>
2023-03-19 17:11:13 +0900

    feat: 「ユースケース層」のサンプルコードを記述

---
+++ chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/infra/ArticleRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.implementingserversidekotlindevelopment.infra
+
+import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * 作成済記事リポジトリの具象クラス
+ *
+ */
+@Repository
+class ArticleRepositoryImpl : ArticleRepository
--- chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/ShowArticleUseCase.kt
+++ chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/ShowArticleUseCase.kt
@@ -1,6 +1,9 @@
 package com.example.implementingserversidekotlindevelopment.usecase
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
 import com.example.implementingserversidekotlindevelopment.domain.CreatedArticle
 import com.example.implementingserversidekotlindevelopment.domain.Slug
 import com.example.implementingserversidekotlindevelopment.util.ValidationError
@@ -44,6 +47,44 @@ interface ShowArticleUseCase {
 /**
  * 作成済記事の単一取得ユースケースの具象クラス
  *
+ * @property articleRepository
  */
 @Service
-class ShowArticleUseCaseImpl : ShowArticleUseCase
+class ShowArticleUseCaseImpl(val articleRepository: ArticleRepository) : ShowArticleUseCase {
+    override fun execute(slug: String?): Either<ShowArticleUseCase.Error, CreatedArticle> {
+        /**
+         * slug の検証
+         *
+         * 不正な slug だった場合、早期 return
+         */
+        val validatedSlug = Slug.new(slug).fold(
+            { return ShowArticleUseCase.Error.ValidationErrors(it.all).left() },
+            { it }
+        )
+
+        /**
+         * 記事の取得
+         */
+        val createdArticle = articleRepository.findBySlug(validatedSlug).fold(
+            /**
+             * 取得失敗
+             */
+            {
+                return when (it) {
+                    /**
+                     * 原因: 記事が見つからなかった
+                     */
+                    is ArticleRepository.FindBySlugError.NotFound -> ShowArticleUseCase.Error.NotFoundArticleBySlug(
+                        validatedSlug
+                    ).left()
+                }
+            },
+            /**
+             * 取得成功
+             */
+            { it }
+        )
+
+        return createdArticle.right()
+    }
+}
