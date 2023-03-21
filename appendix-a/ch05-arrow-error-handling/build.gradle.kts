07ba201c66965c5a3204f1cf11d8d5bb75f044ab
Msksgm <msksgm3124@gmail.com>
2023-03-22 06:33:58 +0900

    build: arrow core を build.gradle.kts に記述

--- appendix-a/ch05-arrow-error-handling/build.gradle.kts
+++ appendix-a/ch05-arrow-error-handling/build.gradle.kts
@@ -13,6 +13,22 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    /**
+     * Arrow Core
+     *
+     * URL
+     * - https://arrow-kt.io/docs/core/
+     * MavenCentral
+     * - https://mvnrepository.com/artifact/io.arrow-kt/arrow-core
+     * Main用途
+     * - Either/Validatedを使ったRailway Oriented Programming
+     * Sub用途
+     * - Optionを使ったletの代替
+     * 概要
+     * - Kotlinで関数型プログラミングをするときに便利なライブラリ
+     */
+    implementation("io.arrow-kt:arrow-core:1.1.5")
 }
 
 tasks.test {
