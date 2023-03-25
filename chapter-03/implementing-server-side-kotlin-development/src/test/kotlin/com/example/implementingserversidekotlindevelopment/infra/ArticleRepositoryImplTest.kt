d9c4244c4f558272bb0ded1a1c3915a03afe482f
kinari321 <abcdream321@yahoo.co.jp>
2023-03-25 18:37:08 +0900

    feat: 「インフラ層」のサンプルコードを記述

--- chapter-03/implementing-server-side-kotlin-development/src/test/kotlin/com/example/implementingserversidekotlindevelopment/infra/ArticleRepositoryImplTest.kt
+++ chapter-03/implementing-server-side-kotlin-development/src/test/kotlin/com/example/implementingserversidekotlindevelopment/infra/ArticleRepositoryImplTest.kt
@@ -1,6 +1,7 @@
 package com.example.implementingserversidekotlindevelopment.infra
 
 import arrow.core.Either
+import arrow.core.right
 import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
 import com.example.implementingserversidekotlindevelopment.domain.Body
 import com.example.implementingserversidekotlindevelopment.domain.CreatedArticle
@@ -159,6 +160,88 @@ class ArticleRepositoryImplTest {
                     assertThat(actual.value.title).isEqualTo(expected.title)
                     assertThat(actual.value.description).isEqualTo(expected.description)
                     assertThat(actual.value.body).isEqualTo(expected.body)
+                }
+            }
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DBRider
+    class Find {
+        @BeforeAll
+        fun reset() = DbConnection.resetSequence()
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/empty-articles.yml"
+            ]
+        )
+        fun `正常系-作成済記事が 1 つもない場合、からの作成済記事の一覧が戻り値`() {
+            /**
+             * given:
+             */
+            val articleRepository = ArticleRepositoryImpl(DbConnection.namedParameterJdbcTemplate)
+
+            /**
+             * when:
+             */
+            val actual = articleRepository.find()
+
+            /**
+             * then:
+             */
+            val expected = listOf<CreatedArticle>().right()
+            assertThat(actual).isEqualTo(expected)
+        }
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/articles.yml"
+            ]
+        )
+        fun `正常系-DB に作成済記事がある場合、全ての作成済記事の一覧が戻り値`() {
+            /**
+             * given:
+             */
+            val articleRepository = ArticleRepositoryImpl(DbConnection.namedParameterJdbcTemplate)
+
+            /**
+             * when:
+             */
+            val actual = articleRepository.find()
+
+            /**
+             * then:
+             */
+            val expected = listOf(
+                CreatedArticle.newWithoutValidation(
+                    slug = Slug.newWithoutValidation("slug0000000000000000000000000001"),
+                    title = Title.newWithoutValidation("dummy-title-01"),
+                    description = Description.newWithoutValidation("dummy-description-01"),
+                    body = Body.newWithoutValidation("dummy-body-01")
+                ),
+                CreatedArticle.newWithoutValidation(
+                    slug = Slug.newWithoutValidation("slug0000000000000000000000000002"),
+                    title = Title.newWithoutValidation("dummy-title-02"),
+                    description = Description.newWithoutValidation("dummy-description-02"),
+                    body = Body.newWithoutValidation("dummy-body-02")
+                ),
+            )
+            when (actual) {
+                is Either.Left -> assert(false)
+                is Either.Right -> {
+                    val createdArticleList = actual.value
+                    assertThat(createdArticleList).hasSameElementsAs(expected)
+                    createdArticleList.forEach { actualArticle ->
+                        val expectedArticle = expected.find { it.slug == actualArticle.slug }!! // 必ず見つかるので !! でキャストする
+                        assertThat(actualArticle.slug).isEqualTo(expectedArticle.slug)
+                        assertThat(actualArticle.title).isEqualTo(expectedArticle.title)
+                        assertThat(actualArticle.body).isEqualTo(expectedArticle.body)
+                        assertThat(actualArticle.description).isEqualTo(expectedArticle.description)
+                    }
                 }
             }
         }
