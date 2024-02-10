481fa564e6ec000cd38b6d2789c0a9cd6ea6390f
Msksgm <49427230+Msksgm@users.noreply.github.com>
2024-02-10 09:45:21 +0900

    feat: arrow 1.2.1 更新に伴い、Validated 型から EitherNel 型へ移行 (#67)
    * build: arrow のバージョンを 1.2.1 に更新
    
    * feat: arrow 1.2.1 に伴い、ValidatedNel によるエラーの累積から、EitherNel によるエラーの累積に変更
    
    * refactor: Validated 型から EitherNel 型の変更に伴い、ファイル名を更新

--- appendix-a/ch05-arrow-error-handling/src/main/kotlin/arrow_eithernel_validation_error.kt
+++ appendix-a/ch05-arrow-error-handling/src/main/kotlin/arrow_eithernel_validation_error.kt
@@ -1,10 +1,11 @@
-package arrow_valid_validation_error
+package arrow_eithernel_validation_error
 
-import arrow.core.Validated
-import arrow.core.ValidatedNel
-import arrow.core.invalidNel
-import arrow.core.valid
-import arrow.core.zip
+import arrow.core.Either
+import arrow.core.EitherNel
+import arrow.core.leftNel
+import arrow.core.raise.either
+import arrow.core.raise.zipOrAccumulate
+import arrow.core.right
 
 fun main() {
     /**
@@ -12,11 +13,11 @@ fun main() {
      * 両方のバリデーションが失敗した結果が返される
      */
     when (val person01 = createPerson("    ", -1)) {
-        is Validated.Invalid -> {
+        is Either.Left -> {
             println("person01 の生成に失敗しました。${person01.value.all.joinToString(separator = ", ") { it.message }}")
         }
 
-        is Validated.Valid -> {
+        is Either.Right -> {
             println("person01 の生成に成功しました。")
             println(person01.value)
         }
@@ -27,11 +28,11 @@ fun main() {
      *  名前のバリデーションが通過して、年齢のバリデーションで失敗したことがわかる
      */
     when (val person02 = createPerson("Alice", -1)) {
-        is Validated.Invalid -> {
+        is Either.Left -> {
             println("person02 の生成に失敗しました。${person02.value.all.joinToString(separator = ", ") { it.message }}")
         }
 
-        is Validated.Valid -> {
+        is Either.Right ->{
             println("person02 の生成に成功しました。")
             println(person02.value)
         }
@@ -41,11 +42,10 @@ fun main() {
      * 名前も年齢も正しいパターン
      */
     when (val person03 = createPerson("Alice", 23)) {
-        is Validated.Invalid -> {
+        is Either.Left -> {
             println("person03 の生成に失敗しました。${person03.value.all.joinToString(separator = ", ") { it.message }}")
         }
-
-        is Validated.Valid -> {
+        is Either.Right -> {
             println("person03 の生成に成功しました。")
             println(person03.value)
         }
@@ -70,15 +70,19 @@ data class Person(
  * @param age
  * @return
  */
-fun createPerson(name: String, age: Int): ValidatedNel<ValidationError, Person> {
+fun createPerson(name: String, age: Int): EitherNel<ValidationError, Person> {
     /**
-     * zip は ValidatedNel が戻り値の関数に対してまとめて検証できる
+     * raise.either と raise.zipOrAccumulate は EitherNel が戻り値の関数に対してまとめて検証できる
+     * Left 型を累積させるには、関数を .bindNel() を付与する必要がある。bind() では累積されないことに注意
      *
-     * 下記の例では、Name と Age のバリデーションをまとめて実施して、失敗だった場合 Nel<ValidationError> 型、成功だった場合 Person 型を返す
+     * 下記の例では、Name と Age のバリデーションをまとめて実施して、失敗だった場合 EitherNel 型、成功だった場合 Person 型を返す
      */
-    return Name.create(name).zip(
-        Age.create(age)
-    ) { validatedName, validatedAge -> Person(validatedName, validatedAge) }
+    return either {
+        zipOrAccumulate(
+            { Name.create(name).bindNel() },
+            { Age.create(age).bindNel() }
+        ) { validatedName, validatedAge -> Person(validatedName, validatedAge) }
+    }
 }
 
 data class ValidationError(val message: String)
@@ -96,14 +100,14 @@ class Name private constructor(val value: String) {
          * @param name
          * @return 失敗:バリデーションエラー、成功:名前オブジェクト
          */
-        fun create(name: String): ValidatedNel<ValidationError, Name> {
+        fun create(name: String): EitherNel<ValidationError, Name> {
             /**
              * 未記入または空白のときバリデーションエラーで早期 return
              */
             if (name.isEmpty() || name.isBlank()) {
-                return ValidationError("名前を入力してください").invalidNel()
+                return ValidationError("名前を入力してください").leftNel()
             }
-            return Name(name).valid()
+            return Name(name).right()
         }
     }
 }
@@ -121,14 +125,14 @@ class Age private constructor(val value: Int) {
          * @param age 年齢
          * @return 失敗:バリデーションエラー、成功:年齢オブジェクト
          */
-        fun create(age: Int): ValidatedNel<ValidationError, Age> {
+        fun create(age: Int): EitherNel<ValidationError, Age> {
             /**
              * サービスの対象外の年齢を入力されたときバリデーションエラー
              */
             if (age < 0 || age > 150) {
-                return ValidationError("年齢は 0 歳以上 150 歳未満で入力してください").invalidNel()
+                return ValidationError("年齢は 0 歳以上 150 歳未満で入力してください").leftNel()
             }
-            return Age(age).valid()
+            return Age(age).right()
         }
     }
 }
