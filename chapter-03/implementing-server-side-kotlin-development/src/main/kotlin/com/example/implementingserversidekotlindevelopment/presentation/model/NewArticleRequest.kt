9b9fa073f7eabc7e7ebf377e829ba74c3818b07c
Msksgm <msksgm3124@gmail.com>
2024-02-23 22:24:06 +0900

    feat: 「プレゼンテーション層」のサンプルコードを記述

---
+++ chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/presentation/model/NewArticleRequest.kt
@@ -0,0 +1,26 @@
+package com.example.implementingserversidekotlindevelopment.presentation.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
+
+/**
+ * 新規作成のリクエストモデル
+ *
+ * @param article
+ */
+data class NewArticleRequest(
+    @field:Valid
+    @Schema(required = true, description = "")
+    @field:NotNull
+    @field:JsonProperty("article", required = true) private val article: NewArticle? = null,
+) {
+    /**
+     * 新規作成記事のプロパティ
+     *
+     * @NotNull アノテーションを付与しているため、null 非許容型に変換
+     */
+    val validatedArticle: NewArticle
+        get() = article!!
+}
