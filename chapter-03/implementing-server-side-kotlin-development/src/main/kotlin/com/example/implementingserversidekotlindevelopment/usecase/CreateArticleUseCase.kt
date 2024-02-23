a17e87d60a88986facae32259696701d892f2068
Msksgm <msksgm3124@gmail.com>
2024-02-23 22:33:31 +0900

    feat: 「ユースケース層」のサンプルコードを記述

--- chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/CreateArticleUseCase.kt
+++ chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/CreateArticleUseCase.kt
@@ -1,6 +1,10 @@
 package com.example.implementingserversidekotlindevelopment.usecase
 
 import arrow.core.Either
+import arrow.core.getOrElse
+import arrow.core.left
+import arrow.core.right
+import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
 import com.example.implementingserversidekotlindevelopment.domain.CreatedArticle
 import com.example.implementingserversidekotlindevelopment.util.ValidationError
 import org.springframework.stereotype.Service
@@ -37,6 +41,23 @@ interface CreateArticleUseCase {
 /**
  * 作成済記事の作成ユースケース取得クラス
  *
+ * @property articleRepository
  */
 @Service
-class CreateArticleUseCaseImpl : CreateArticleUseCase
+class CreateArticleUseCaseImpl(val articleRepository: ArticleRepository) : CreateArticleUseCase {
+    override fun execute(title: String, description: String, body: String): Either<CreateArticleUseCase.Error, CreatedArticle> {
+        /**
+         * 作成済記事オブジェクトの作成
+         *
+         * バリデーションエラーが発生した場合、早期リターン
+         */
+        val uncreatedArticle = CreatedArticle.new(title, description, body).getOrElse { return CreateArticleUseCase.Error.InvalidArticle(it).left() }
+
+        /**
+         * 作成済記事を保存
+         */
+        val createdArticle = articleRepository.create(createdArticle = uncreatedArticle).getOrElse { throw UnsupportedOperationException("現在この分岐に入ることはない") }
+
+        return createdArticle.right()
+    }
+}
