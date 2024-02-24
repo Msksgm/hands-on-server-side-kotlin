e6de4f8e6c61250f4d4f238059fb2c48569016a7
Msksgm <msksgm3124@gmail.com>
2024-02-24 14:29:06 +0900

    feat: 「ユースケース層の実装」のサンプルコードを記述

--- chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/UpdateArticleUseCase.kt
+++ chapter-03/implementing-server-side-kotlin-development/src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/UpdateArticleUseCase.kt
@@ -1,8 +1,13 @@
 package com.example.implementingserversidekotlindevelopment.usecase
 
 import arrow.core.Either
+import arrow.core.getOrElse
+import arrow.core.left
+import arrow.core.right
+import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
 import com.example.implementingserversidekotlindevelopment.domain.CreatedArticle
 import com.example.implementingserversidekotlindevelopment.domain.Slug
+import com.example.implementingserversidekotlindevelopment.domain.UpdatableCreatedArticle
 import com.example.implementingserversidekotlindevelopment.util.ValidationError
 import org.springframework.stereotype.Service
 
@@ -52,6 +57,30 @@ interface UpdateArticleUseCase {
 /**
  * 記事更新ユースケースの具象クラス
  *
+ * @property articleRepository
  */
 @Service
-class UpdateArticleUseCaseImpl : UpdateArticleUseCase
+class UpdateArticleUseCaseImpl(val articleRepository: ArticleRepository) : UpdateArticleUseCase {
+    override fun execute(slug: String, title: String, description: String, body: String): Either<UpdateArticleUseCase.Error, CreatedArticle> {
+        /**
+         * slug のバリデーション
+         *
+         * 不正だった場合、早期 return
+         */
+        val validatedSlug = Slug.new(slug).getOrElse { return UpdateArticleUseCase.Error.ValidationErrors(it).left() }
+
+        /**
+         * 更新用作成済記事の生成
+         *
+         * 不正だった場合、早期 return
+         */
+        val unsavedUpdatableArticle = UpdatableCreatedArticle.new(title, description, body).getOrElse { return UpdateArticleUseCase.Error.ValidationErrors(it).left() }
+
+        /**
+         * 作成済記事の更新
+         */
+        val createdArticle = articleRepository.update(validatedSlug, unsavedUpdatableArticle).getOrElse { return UpdateArticleUseCase.Error.NotFoundArticleBySlug(validatedSlug).left() }
+
+        return createdArticle.right()
+    }
+}
